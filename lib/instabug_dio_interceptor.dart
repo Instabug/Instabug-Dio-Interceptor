6d8ab2c70f5807a377086d6c11b4845f3970955e
Aly ezz <alyezz94@gmail.com>
2019-07-29 14:51:24 +0200

    💎 Bump to version 1.0.0

--- lib/instabug_dio_interceptor.dart
+++ lib/instabug_dio_interceptor.dart
@@ -1,9 +1,8 @@
 import 'package:dio/dio.dart';
-import 'package:instabug/models/network_data.dart';
-import 'package:instabug/NetworkLogger.dart';
+import 'package:instabug_flutter/models/network_data.dart';
+import 'package:instabug_flutter/NetworkLogger.dart';
 
 class InstabugDioInterceptor extends Interceptor {
-
   static final Map<int, NetworkData> _requests = <int, NetworkData>{};
 
   @override
@@ -33,7 +32,8 @@ class InstabugDioInterceptor extends Interceptor {
   NetworkData _map(Response response) {
     final NetworkData data = _getRequestData(response.request.hashCode);
     data.endTime = DateTime.now();
-    data.duration = data.endTime.millisecondsSinceEpoch - data.startTime.millisecondsSinceEpoch;
+    data.duration = data.endTime.millisecondsSinceEpoch -
+        data.startTime.millisecondsSinceEpoch;
     final Map<String, dynamic> responseHeaders = <String, dynamic>{};
     response.headers.forEach((name, value) => responseHeaders[name] = value);
     data.url = response.request.uri.toString();
@@ -42,9 +42,8 @@ class InstabugDioInterceptor extends Interceptor {
     data.requestHeaders = response.request.headers;
     data.contentType = response.request.contentType.toString();
     data.status = response.statusCode;
-    data.responseBody = response.data;   
-    data.responseHeaders = responseHeaders; 
+    data.responseBody = response.data;
+    data.responseHeaders = responseHeaders;
     return data;
   }
-
 }
